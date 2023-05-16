60106008f9d3906e8afd57c8cb33bc0b8e5b0f37
luiz pucca <lu1z_21@hotmail.com>
2023-05-16 20:58:29 -0300

    adjust layout

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -7,40 +7,42 @@ import NotificationIcon from "../../assets/CircleNotifications.svg";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="headerLef">
-        <Image width={30} height={30} src={MenuIcon} alt="menu" />
-        <Image
-          width={108}
-          height={32}
-          src={LogoIcon}
-          alt="logo-proz"
-          style={{ marginLeft: 18 }}
-        />
-      </div>
-      <div className="headerRight">
-        <div className="notification"></div>
-        <Image
-          width={24}
-          height={24}
-          src={NotificationIcon}
-          alt="notificações"
-        />
-        <div className="wrapperPerfilImage">
+    <div className="headercontainer">
+      <header className="header">
+        <div className="headerLef">
+          <Image width={30} height={30} src={MenuIcon} alt="menu" />
           <Image
-            width={40}
-            height={40}
-            src={PerfilImage}
-            alt="perfil"
-            style={{ borderRadius: "50%", objectFit: "cover" }}
+            width={108}
+            height={32}
+            src={LogoIcon}
+            alt="logo-proz"
+            style={{ marginLeft: 18 }}
           />
         </div>
-        <div className="wrapperPerfinInfos">
-          <span className="perfilName">Joao da silva</span>
-          <div className="perfilProfession">Professor</div>
+        <div className="headerRight">
+          <div className="notification"></div>
+          <Image
+            width={24}
+            height={24}
+            src={NotificationIcon}
+            alt="notificações"
+          />
+          <div className="wrapperPerfilImage">
+            <Image
+              width={40}
+              height={40}
+              src={PerfilImage}
+              alt="perfil"
+              style={{ borderRadius: "50%", objectFit: "cover" }}
+            />
+          </div>
+          <div className="wrapperPerfinInfos">
+            <span className="perfilName">Joao da silva</span>
+            <div className="perfilProfession">Professor</div>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
